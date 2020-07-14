738120ce2296c6c68bd0e1ace077f4a5424b52a6
erez.efrat <erez.efrat@overwolf.com>
2020-07-14 18:32:31 +0300

    Updates

--- precache-manifest.288bb93239b2b96d2059b9d5c0a7aaba.js
+++ precache-manifest.288bb93239b2b96d2059b9d5c0a7aaba.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "600671b6571131e3ceb3867d59b5847f",
+    "revision": "f3b7137f4e88cc343b034ae2893d5123",
     "url": "/Erez-Efrat-14-07-2020/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Erez-Efrat-14-07-2020/static/js/2.b5b9337b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "87f3c28d992f6d7bd9ce",
-    "url": "/Erez-Efrat-14-07-2020/static/js/main.8ec6b7b8.chunk.js"
+    "revision": "fe8868726977fbe1dd30",
+    "url": "/Erez-Efrat-14-07-2020/static/js/main.6fe2a60b.chunk.js"
   },
   {
     "revision": "ffcee6ab58dd777c31ef",
